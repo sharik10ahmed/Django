5f6c470c8f68f3753986ba69ef4f0841879f8cf8
sharik10ahmed <sharikahmed757@gmail.com>
2026-04-01 22:42:01 +0530

    added

--- my_first_project/my_first_project/old_urls.py
+++ my_first_project/my_first_project/old_urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myproject project.
+URL configuration for my_first_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -15,16 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from django.conf import settings
-from django.conf.urls.static import static
-
-
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/',include('Newapp.urls'))
 ]
-
-
-urlpatterns += static( settings.MEDIA_URL ,document_root = settings.MEDIA_ROOT)
