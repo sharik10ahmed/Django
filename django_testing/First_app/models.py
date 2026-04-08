0e625b06bbb56991ba4e8de2091ec1ff308c70e4
sharik10ahmed <sharikahmed757@gmail.com>
2026-04-08 19:11:21 +0530

    added

--- django_testing/First_app/models.py
+++ django_testing/First_app/models.py
@@ -1,3 +1,13 @@
-from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=255)
+    product_price = models.DecimalField(max_digits=10, decimal_places=2)
+    product_description = models.TextField()
+    product_image = models.ImageField(upload_to='products/')
+
+    def __str__(self):
+        return self.product_name
