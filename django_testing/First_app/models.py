9790638478630e872dcd34cdf25fa8cb3edaa444
sharik10ahmed <sharikahmed757@gmail.com>
2026-04-11 18:57:45 +0530

    added

--- django_testing/First_app/models.py
+++ django_testing/First_app/models.py
@@ -1,13 +1,22 @@
 
 # Create your models here.
 
-from django.db import models
+from django.db import models  
+class Category(models.Model):
+    category_name = models.CharField(max_length=255)
+    category_description = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.category_name
+    
 class Product(models.Model):
     product_name = models.CharField(max_length=255)
     product_price = models.DecimalField(max_digits=10, decimal_places=2)
     product_description = models.TextField()
     product_image = models.ImageField(upload_to='products/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
 
     def __str__(self):
         return self.product_name
+    
+    category = models.ManyToManyField(Category, related_name='products')
--- django_testing/django_testing/settings.py
+++ django_testing/django_testing/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'django_testing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_product',
+        'USER': 'postgres',
+        'PASSWORD': '731024', 
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
