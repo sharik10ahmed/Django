1012d15f6b217812b989c94c44f7ee6010b6a22e
sharik10ahmed <sharikahmed757@gmail.com>
2026-04-30 21:33:57 +0530

    added

---
+++ crud_practice/cruds/cruds_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CrudsAppConfig(AppConfig):
+    name = 'cruds_app'
---
+++ crud_practice/cruds/cruds_app/models.py
@@ -0,0 +1,13 @@
+
+##insert data
+from django.db import models
+
+class Student(models.Model):
+    std_name = models.CharField(max_length=100)
+    std_roll = models.IntegerField()
+    std_image = models.ImageField(upload_to='students/')
+    std_email = models.EmailField()
+    std_city = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.std_name
